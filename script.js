e4f40002a86a34968c42ec4a015f22533c5b8c5d
mateus fernandes <mateusfernandesrosa@gmail.com>
2025-12-30 10:56:00 -0300

    Atualização do projeto

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function logar() {
+  var login = document.getElementById("username").value;
+  var senha = document.getElementById("password").value;
+
+  if (login === "admin" && senha === "123") {
+    alert("Sucesso!");
+    window.location.href = "home.html";
+    return false; 
+  } else {
+    alert("Usuário ou senha incorretas");
+    return false;
+  }
+}
+
+document.getElementById("btnCadastrarLivro").onclick = function(){
+    window.location.href = "cadastrar_livro.html";
+};
+
+document.getElementById("btnVerLivros").onclick = function(){
+    window.location.href = "lista_livros.html";
+};
+
+document.getElementById("btnCadastrarCliente").onclick = function(){
+    window.location.href = "cadastrar_cliente.html";
+};
+
+document.getElementById("btnAlugarLivro").onclick = function(){
+    window.location.href = "alugar_livro.html";
+};
+
+document.getElementById("btnDevolverLivro").onclick = function(){
+    window.location.href = "devolver_livro.html";
+};
+
+// Exemplo de logout
+document.getElementById("btnSair").onclick = function(){
+    alert("Saindo do sistema...");
+    window.location.href = "login.html";
+};
